ca583dca93869aad10015bd0a76eda9100434e0a
kfogel <kfogel@a359aad5-c104-ce93-c58f-3d496fa64091>
2010-09-29 23:38:31 +0000

    * peoplepods/pods/mediabugs_authentication/settings.php: Remove unneeded   leading slash on URL path component, as reported to Ben Brown.

--- peoplepods/pods/mediabugs_authentication/settings.php
+++ peoplepods/pods/mediabugs_authentication/settings.php
@@ -1,6 +1,6 @@
 <? 
 
 	$POD->registerPOD('mediabugs_authentication_login','Allows Login, Logout, Password Reset',array('^r/(.*)'=>'mediabugs_authentication/login.php?checklogin=1&redirect=/$1','^login'=>'mediabugs_authentication/login.php','^logout'=>'mediabugs_authentication/logout.php','^password_reset/(.*)'=>'mediabugs_authentication/password.php?resetCode=$1','^password_reset$'=>'mediabugs_authentication/password.php'),array());
-	$POD->registerPOD('mediabugs_authentication_creation','Allows new members to join your site',array('^join'=>'/mediabugs_authentication/join.php','^verify'=>'mediabugs_authentication/verify.php'),array());
+	$POD->registerPOD('mediabugs_authentication_creation','Allows new members to join your site',array('^join'=>'mediabugs_authentication/join.php','^verify'=>'mediabugs_authentication/verify.php'),array());
 
 ?>
