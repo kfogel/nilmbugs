1aaea4165d72b7fc97cbf49eb754cf345d3bda6b
webchickbot <webchickbot@a359aad5-c104-ce93-c58f-3d496fa64091>
2010-11-21 22:58:45 +0000

    more revisions to support customized version of flowplayer - svn commit did not push all files when in /img/ directory again

--- trunk/themes/nilmbugs/header.php
+++ trunk/themes/nilmbugs/header.php
@@ -35,6 +35,9 @@
 	<script type="text/javascript" src="<? $POD->templateDir(); ?>/js/jquery-datepick/jquery.datepick.js"></script>
 	<script type="text/javascript" src="<? $POD->templateDir(); ?>/js/jquery-datepick/jquery.datepick-validation.js"></script>
 
+	<link rel="stylesheet" type="text/css" href="<? $POD->templateDir(); ?>/js/flowplayer.css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="<? $POD->templateDir(); ?>/js/flowplayer-3.2.4.min.js" media="screen" charset="utf-8" />
+
 
 	<link rel="stylesheet" type="text/css" href="<? $POD->templateDir(); ?>/js/jquery-bt/jquery.bt.css" media="screen" charset="utf-8" />
 	<link rel="stylesheet" type="text/css" href="<? $POD->templateDir(); ?>/js/jquery-autocomplete/jquery.autocomplete.css" media="screen" charset="utf-8" />
