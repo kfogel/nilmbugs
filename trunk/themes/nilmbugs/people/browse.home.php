0ad6ee8f7550fa424e50f89a2d37cad19c945652
webchickbot <webchickbot@a359aad5-c104-ce93-c58f-3d496fa64091>
2010-11-22 00:54:11 +0000

    adding tweetstream to home page

--- trunk/themes/nilmbugs/people/browse.home.php
+++ trunk/themes/nilmbugs/people/browse.home.php
@@ -67,6 +67,38 @@ $media_outlets = $POD->mediaOutlets(9);
 	
 	<? $POD->output('sidebars/recent_bugs'); ?>
 
-	<? $POD->output('sidebars/browse'); ?>
-
+<script src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'search',
+  search: 'lawgov, law.gov',
+  interval: 6000,
+  title: '',
+  subject: '#law.gov',
+  width: 250,
+  height: 300,
+  theme: {
+    shell: {
+      background: '#ffffff',
+      color: '#1986b5'
+    },
+    tweets: {
+      background: '#ffffff',
+      color: '#444444',
+      links: '#1986b5'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: true,
+    live: true,
+    hashtags: true,
+    timestamp: true,
+    avatars: true,
+    toptweets: true,
+    behavior: 'default'
+  }
+}).render().start();
+</script>
 </div>
