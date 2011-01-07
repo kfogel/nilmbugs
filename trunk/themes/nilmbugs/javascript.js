cbdfdf203f9e3b8bbaad77681bd9a779ab3618f7
webchickbot <webchickbot@a359aad5-c104-ce93-c58f-3d496fa64091>
2011-01-07 23:46:23 +0000

    changing tooltip font

--- trunk/themes/nilmbugs/javascript.js
+++ trunk/themes/nilmbugs/javascript.js
@@ -70,7 +70,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#FFF', fontWeight: 'bold'}			
+			 cssStyles: {color: '#FFF', fontWeight: 'bold', fontFamily: 'Arial, Helvetica'}			
 			}
 		);
 		$('p.input input').bt( {
@@ -84,7 +84,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#C0B770', fontWeight: 'normal'}
+			 cssStyles: {color: '#C0B770', fontWeight: 'normal', fontFamily: 'Arial, Helvetica'}
 			}
 		);
 		$('p.input textarea').bt( {
@@ -98,7 +98,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#C0B770', fontWeight: 'normal'}
+			 cssStyles: {color: '#C0B770', fontWeight: 'normal', fontFamily: 'Arial, Helvetica'}
 			}
 		);
 
