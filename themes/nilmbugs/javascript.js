0f5a6f5ce6966234509c70a9d330b85b4c7cbe75
webchickbot <webchickbot@a359aad5-c104-ce93-c58f-3d496fa64091>
2011-01-07 23:51:26 +0000

    remove bold weight

--- themes/nilmbugs/javascript.js
+++ themes/nilmbugs/javascript.js
@@ -84,7 +84,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#C0B770', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '11px'}
+			 cssStyles: {color: '#C0B770', fontWeight: 'normal', fontFamily: 'Arial, Helvetica', fontSize: '11px'}
 			}
 		);
 		$('p.input textarea').bt( {
@@ -98,7 +98,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#C0B770', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '11px'}
+			 cssStyles: {color: '#C0B770', fontWeight: 'normal', fontFamily: 'Arial, Helvetica', fontSize: '11px'}
 			}
 		);
 
