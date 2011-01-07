eda39069dde98f844584ad29a79467faa96c60ff
webchickbot <webchickbot@a359aad5-c104-ce93-c58f-3d496fa64091>
2011-01-07 23:50:34 +0000

    font size tweak

--- trunk/themes/nilmbugs/javascript.js
+++ trunk/themes/nilmbugs/javascript.js
@@ -70,7 +70,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#FFF', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '10px'}			
+			 cssStyles: {color: '#FFF', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '11px'}			
 			}
 		);
 		$('p.input input').bt( {
@@ -84,7 +84,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#C0B770', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '10px'}
+			 cssStyles: {color: '#C0B770', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '11px'}
 			}
 		);
 		$('p.input textarea').bt( {
@@ -98,7 +98,7 @@
 		    shadowColor: 'rgba(0,0,0,.6)',
 		    shadowOverlap: false,	
 		    padding:10,
-			 cssStyles: {color: '#C0B770', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '10px'}
+			 cssStyles: {color: '#C0B770', fontWeight: 'bold', fontFamily: 'Arial, Helvetica', fontSize: '11px'}
 			}
 		);
 
